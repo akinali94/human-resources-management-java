4e511e0ae3bd0bd66ea5d6a02ddd2a217d74373c
akinali94 <aliakn.20@gmail.com>
2025-10-29 13:53:40 +0300

    added first login page template

---
+++ humanresources-ui/src/main/resources/static/assets/js/navbarwrapper.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    $('.allowance-wrapper').hover(
+        function () {
+            // Mouse enters the allowance-wrapper
+            $(this).find('.allowanceSubmenu').show();
+        },
+        function () {
+            // Mouse leaves the allowance-wrapper
+            $(this).find('.allowanceSubmenu').hide();
+        }
+    );
+});
